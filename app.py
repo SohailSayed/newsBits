dda50f7133be991ea68db2de207d8d08ff3cf9dd
sohailsayed <s7sayed@uwaterloo.ca>
2022-11-14 00:58:32 -0500

    remove 1024 char cap for article being summarized and create new template for article summary

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 app.secret_key = "testkey"
 
+def summarize_text(text: str, max_len: int) -> str:
+    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+    try:
+        summary = summarizer(text, max_length=max_len, min_length=10, do_sample=False)
+        return summary[0]["summary_text"]
+    except IndexError as ex:
+        print(ex)
+        return summarize_text(text=text[:(len(text) // 2)], max_len=max_len//2) + summarize_text(text=text[(len(text) // 2):], max_len=max_len//2)
+
 @app.route("/", methods=["POST", "GET"])
 def home():
     if request.method == "POST":
@@ -19,25 +28,33 @@ def home():
             response = requests.get(url)
             parsedResponse = response.json()['articles']
             articleURL = parsedResponse[0]['url']
+            title = parsedResponse[0]['title']
+            session["articleURL"] = articleURL
+            session["title"] = title
             r = requests.get(articleURL)
             soup = BeautifulSoup(r.content, 'html.parser')
             originalArticle = soup.find("div", class_ = "story").get_text()
+            lent = len(originalArticle)
 
-            summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-            summary = summarizer(originalArticle, max_length=130, min_length=30, do_sample=False)
-            session["summary"] = summary[0]['summary_text']
+            summary = summarize_text(originalArticle, 130)
+
+            session["summary"] = summary
         return redirect(url_for("source"))
     else:
         return render_template("index.html")
 
 @app.route("/source")
 def source():
-    # if "source" in session:
-    #     source = session["source"]
-    #     return "<h1>You have selected {source}</h1>".format(source)
+    if "title" in session:
+        title = session["title"]
+    if "articleURL" in session:
+        articleURL = session["articleURL"]
+    if "source" in session:
+        source = session["source"]
     if "summary" in session:
         summary = session["summary"]
-        return "<p>{0}</p>".format(summary)
+        return render_template("summary.html", source = source, summary = summary, articleURL = articleURL, title=title)
+        # return "<p>{0}</p>".format(summary)
     else:
         return redirect(url_for(home))
 
