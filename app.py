5f1f8edf49a2f63adbbd7f50228225a14a876c7f
sohailsayed <s7sayed@uwaterloo.ca>
2022-11-21 21:54:00 -0500

    summary page now displays multiple article summaries from the day

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ from bs4 import BeautifulSoup
 from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
-app.secret_key = "testkey"
+app.secret_key = "testytest"
 
 def summarize_text(text: str, max_len: int) -> str:
     summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
     try:
         summary = summarizer(text, max_length=max_len, min_length=10, do_sample=False)
         return summary[0]["summary_text"]
-    except IndexError as ex:
-        print(ex)
+    # Might need to specify this exception to handle IndexError and tensorflow.python.framework.errors_impl.InvalidArgumentError somehow (second one is hard, gotta figure out)
+    except:
         return summarize_text(text=text[:(len(text) // 2)], max_len=max_len//2) + summarize_text(text=text[(len(text) // 2):], max_len=max_len//2)
 
 @app.route("/", methods=["POST", "GET"])
@@ -22,39 +22,48 @@ def home():
         session["source"] = source
 
         if source == "cbc":
+            # Everything below probably needs to be made into a seperate function soon
             url = ('https://newsapi.org/v2/top-headlines?'
                 'sources=cbc-news&'
                 'apiKey=934c6a1a98ca46efa144a9957f555fdb')
             response = requests.get(url)
             parsedResponse = response.json()['articles']
-            articleURL = parsedResponse[0]['url']
-            title = parsedResponse[0]['title']
-            session["articleURL"] = articleURL
-            session["title"] = title
-            r = requests.get(articleURL)
-            soup = BeautifulSoup(r.content, 'html.parser')
-            originalArticle = soup.find("div", class_ = "story").get_text()
-            lent = len(originalArticle)
 
-            summary = summarize_text(originalArticle, 130)
+            titles = []
+            urls = []
+            summaries = []
+            for article in parsedResponse:
+                titles.append(article['title'])
+                urls.append(article['url'])
+                r = requests.get(article['url'])
+                soup = BeautifulSoup(r.content, 'html.parser')
 
-            session["summary"] = summary
+                try:
+                    originalArticle = soup.find("div", class_ = "story").get_text()
+                except AttributeError:
+                    titles.pop()
+                    urls.pop()
+                    pass
+                
+                summaries.append(summarize_text(originalArticle, 130))
+            session["titles"] = titles
+            session["urls"] = urls
+            session["summaries"] = summaries
         return redirect(url_for("source"))
     else:
         return render_template("index.html")
 
 @app.route("/source")
 def source():
-    if "title" in session:
-        title = session["title"]
-    if "articleURL" in session:
-        articleURL = session["articleURL"]
+    if "titles" in session:
+        titles = session["titles"]
+    if "urls" in session:
+        urls = session["urls"]
+    if "summaries" in session:
+        summaries = session["summaries"]
     if "source" in session:
         source = session["source"]
-    if "summary" in session:
-        summary = session["summary"]
-        return render_template("summary.html", source = source, summary = summary, articleURL = articleURL, title=title)
-        # return "<p>{0}</p>".format(summary)
+        return render_template("summary.html", source=source, titles=titles, urls=urls, summaries=summaries)
     else:
         return redirect(url_for(home))
 
