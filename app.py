1f556ce53e6e0413ef951e18fb87fc4c62602ddc
sohailsayed <s7sayed@uwaterloo.ca>
2022-11-23 23:12:17 -0500

    add 4 new news sources

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ def summarize_text(text: str, max_len: int) -> str:
     try:
         summary = summarizer(text, max_length=max_len, min_length=10, do_sample=False)
         return summary[0]["summary_text"]
-    # Might need to specify this exception to handle IndexError and tensorflow.python.framework.errors_impl.InvalidArgumentError somehow (second one is hard, gotta figure out)
     except:
         return summarize_text(text=text[:(len(text) // 2)], max_len=max_len//2) + summarize_text(text=text[(len(text) // 2):], max_len=max_len//2)
 
@@ -21,21 +20,38 @@ def home():
         source = request.form['sources']
         session["source"] = source
 
-        if source == "cbc":
-            # Everything below probably needs to be made into a seperate function soon
+        #This is a rudimentary way of calling the selected source, must be changed soon
+        if source == "CBC":
             url = ('https://newsapi.org/v2/top-headlines?'
                 'sources=cbc-news&'
                 'apiKey=934c6a1a98ca46efa144a9957f555fdb')
-            response = requests.get(url)
-            parsedResponse = response.json()['articles']
+        if source == "CNN":
+            url = ('https://newsapi.org/v2/top-headlines?'
+                'sources=cnn&'
+                'apiKey=934c6a1a98ca46efa144a9957f555fdb')
+        if source == "BBC":
+            url = ('https://newsapi.org/v2/top-headlines?'
+                'sources=bbc-news&'
+                'apiKey=934c6a1a98ca46efa144a9957f555fdb')
+        if source == "Reuters":
+            url = ('https://newsapi.org/v2/top-headlines?'
+                'sources=reuters&'
+                'apiKey=934c6a1a98ca46efa144a9957f555fdb')
+        if source == "Associated Press":
+            url = ('https://newsapi.org/v2/top-headlines?'
+                'sources=associated-press&'
+                'apiKey=934c6a1a98ca46efa144a9957f555fdb')
+        
+        response = requests.get(url)
+        parsedResponse = response.json()['articles']
 
-            titles = []
-            urls = []
-            for article in parsedResponse:
-                titles.append(article['title'])
-                urls.append(article['url'])
-            session["titles"] = titles
-            session["urls"] = urls
+        titles = []
+        urls = []
+        for article in parsedResponse:
+            titles.append(article['title'])
+            urls.append(article['url'])
+        session["titles"] = titles
+        session["urls"] = urls
         return redirect(url_for("source"))
     else:
         return render_template("index.html")
