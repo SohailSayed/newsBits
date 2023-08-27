f82ecb91427d0c9702cee11504383b47b0eae4d6
sohailsayed <s7sayed@uwaterloo.ca>
2023-08-27 02:31:41 -0400

    add error messaging for bad requests

--- modules/summarizer.py
+++ modules/summarizer.py
@@ -5,9 +5,13 @@ import requests
 
 def summarize_text(text: str, max_len: int) -> str:
     try:
-        summary = query({"inputs" : text, "max_length" : max_len,})
+        response = query({"inputs" : text, "max_length" : max_len,})
+        if not response.ok:
+            return "Condensed.news is currently experiening some issues in the backend. After being resolved, article summaries will display here again within the next two hours. I appreciate your patience!"
+        summary = response.json()
         return summary[0]["summary_text"]
-    except:
+    except Exception as e:
+        print(e)
         return summarize_text(text=text[:(len(text) // 2)], max_len=max_len//2) + summarize_text(text=text[(len(text) // 2):], max_len=max_len//2)
 
 def query(payload):
@@ -16,5 +20,5 @@ def query(payload):
     headers = {"Authorization": "Bearer {}".format(os.getenv('INFERENCE_API_KEY'))}
     API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
     response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()
+    return response
 
